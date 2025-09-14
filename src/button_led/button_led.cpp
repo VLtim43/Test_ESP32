9431328f1e8b19911f852d08b02666cc82962970
VLtim43 <fernandoreal.valentim@gmail.com>
2025-09-14 19:30:45 -0300

    feat: button led

---
+++ src/button_led/button_led.cpp
@@ -0,0 +1,24 @@
+// #include <Arduino.h>
+
+// #define LED 2
+// #define BUTTON 13
+
+// void setup() {
+//   Serial.begin(9600);
+//   pinMode(LED, OUTPUT);
+//   pinMode(BUTTON, INPUT_PULLUP);
+// }
+
+// void loop() {
+//   bool buttonStatus = digitalRead(BUTTON);
+
+//   if (buttonStatus == LOW) {
+//     digitalWrite(LED, HIGH);  // Turn LED on
+//     Serial.println("Button pressed - LED ON");
+//   } else {
+//     digitalWrite(LED, LOW);  // Turn LED off
+//     Serial.println("Button released - LED OFF");
+//   }
+
+//   delay(100);
+// }
