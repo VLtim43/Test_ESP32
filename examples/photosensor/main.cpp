0e2d71862ad5c81e385fa7b106539c15f596c552
VLtim43 <fernandoreal.valentim@gmail.com>
2025-09-19 12:28:24 -0300

    led

--- examples/photosensor/main.cpp
+++ examples/photosensor/main.cpp
@@ -1,33 +1,49 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include <Arduino.h>
+#include <Wire.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 
 const int PHOTORESISTOR_DIGITAL_PIN = 12;
-const int DAY_LED_PIN = 5;
-const int NIGHT_LED_PIN = 4;
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
 
   pinMode(PHOTORESISTOR_DIGITAL_PIN, INPUT);
-  pinMode(DAY_LED_PIN, OUTPUT);
-  pinMode(NIGHT_LED_PIN, OUTPUT);
 
-  Serial.println("Photosensor Digital Day/Night LED Example");
-  Serial.println("Reading digital light state...");
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;);
+  }
+
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println("Light");
+  display.println("Sensor");
+  display.display();
+  delay(2000);
 }
 
 void loop() {
   int digitalState = digitalRead(PHOTORESISTOR_DIGITAL_PIN);
 
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setCursor(0, 10);
+
   if (digitalState == HIGH) {
-    digitalWrite(NIGHT_LED_PIN, HIGH);
-    digitalWrite(DAY_LED_PIN, LOW);
-    Serial.println("◐ NIGHT");
+    display.println("NIGHT");
   } else {
-    digitalWrite(DAY_LED_PIN, HIGH);
-    digitalWrite(NIGHT_LED_PIN, LOW);
-    Serial.println("☼ DAY");
+    display.println("DAY");
   }
 
+  display.display();
   delay(1000);
 }
